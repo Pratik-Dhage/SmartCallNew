1dd5a4ed31bae00d691936d888209669ac550ecc
Pratik Dhage <dhagepratik94@gmail.com>
2023-01-10 17:24:38 +0530

    register password activity design added

--- app/src/main/java/com/example/test/otp/OTPVerifyActivity.java
+++ app/src/main/java/com/example/test/otp/OTPVerifyActivity.java
@@ -1,16 +1,55 @@
 package com.example.test.otp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.test.R;
+import com.example.test.databinding.ActivityOtpverifyBinding;
+import com.example.test.login.LoginActivity;
+import com.example.test.register_password.RegisterPasswordActivity;
 
 public class OTPVerifyActivity extends AppCompatActivity {
+
+    ActivityOtpverifyBinding binding;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otpverify);
+
+        initializeFields();
+        onClickListener();
+    }
+
+    private void initializeFields() {
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_otpverify);
+        view = binding.getRoot();
+    }
+
+    private void onClickListener() {
+
+        if(binding.edtOTP.getText().toString().isEmpty()){
+
+        }
+        else{
+
+            binding.btnVerifyOTP.setBackgroundColor(getResources().getColor(R.color.textBlue));
+            binding.btnVerifyOTP.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent i = new Intent(OTPVerifyActivity.this, RegisterPasswordActivity.class);
+                    startActivity(i);
+
+
+                }
+            });
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/test/register_password/RegisterPasswordActivity.java
@@ -0,0 +1,47 @@
+package com.example.test.register_password;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.test.R;
+import com.example.test.SuccessActivity;
+import com.example.test.databinding.ActivityRegisterPasswordBinding;
+import com.example.test.login.LoginActivity;
+import com.example.test.otp.OTPActivity;
+
+public class RegisterPasswordActivity extends AppCompatActivity {
+
+    ActivityRegisterPasswordBinding binding;
+    View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initializeFields();
+        onClickListener();
+    }
+
+    private void initializeFields() {
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_register_password);
+        view = binding.getRoot();
+    }
+
+    private void onClickListener() {
+        binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(RegisterPasswordActivity.this, SuccessActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
+
+
+}
