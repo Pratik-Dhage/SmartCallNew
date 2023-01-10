e9225b078340db04e6e7e3f01576cc57e51297f0
Pratik Dhage <dhagepratik94@gmail.com>
2023-01-10 15:11:25 +0530

    login screen edit text and textView size changes

--- app/src/main/java/com/example/test/login/LoginActivity.java
+++ app/src/main/java/com/example/test/login/LoginActivity.java
@@ -1,16 +1,48 @@
 package com.example.test.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.test.R;
+import com.example.test.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        initializeFields();
+        onClickListener();
     }
+
+    private void initializeFields() {
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+        view = binding.getRoot();
+    }
+
+    private void onClickListener() {
+
+        binding.txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
 }
