fd46ca2f606a906211058439dcc45c9f0b123a3f
Pratik Dhage <dhagepratik94@gmail.com>
2023-01-13 18:27:45 +0530

    big button style added and LeadsViewModel api code added and commented

--- app/src/main/java/com/example/test/lead/LeadsViewModel.java
+++ app/src/main/java/com/example/test/lead/LeadsViewModel.java
@@ -2,5 +2,31 @@ package com.example.test.lead;
 
 import androidx.lifecycle.ViewModel;
 
+import io.reactivex.disposables.Disposable;
+
 public class LeadsViewModel extends ViewModel {
+
+
+    private Disposable subscribtion; //Disposable Interface used to prevent observer from receiving items from Observer before all items are loaded.
+
+
+    //Call to API with Required Function
+
+ /*   subscribtion = (Disposable) Global.apiService().getAllProducts(WebServices.Domain2+WebServices.allProducts)
+                  .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .unsubscribeOn(Schedulers.io())
+            .subscribe(
+                          this::onHomeApiSuccess , this::onApiError
+                          );
+
+    private void onHomeApiSuccess(HomeResponse result) {
+        mutHomeResponseApi.setValue(result);
+    }
+
+    private void onApiError(Throwable error) {
+        mutErrorResponse.setValue(error.getLocalizedMessage());
+    }
+ */
+
 }
