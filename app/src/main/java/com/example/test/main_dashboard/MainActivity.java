4bd4df65287331266556d5cf3a60b41fba64e4ea
Pratik Dhage <dhagepratik94@gmail.com>
2023-02-22 17:24:24 +0530

    Main DashBoard Changes

--- app/src/main/java/com/example/test/main_dashboard/MainActivity.java
+++ app/src/main/java/com/example/test/main_dashboard/MainActivity.java
@@ -10,13 +10,20 @@ import android.view.View;
 
 import com.example.test.R;
 import com.example.test.databinding.ActivityMainBinding;
+import com.example.test.databinding.ActivityMainUpdatedBinding;
 import com.example.test.lead.LeadsActivity;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 //This is Dashboard Activity
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    ActivityMainUpdatedBinding binding;
     View view;
     MainDashBoardViewModel mainDashBoardViewModel;
 
@@ -26,14 +33,25 @@ public class MainActivity extends AppCompatActivity {
 
         initializeFields();
         onClickListener();
+        setUpDashboardData();
     }
 
     private void initializeFields() {
-     binding = DataBindingUtil.setContentView(this, R.layout.activity_main) ;
+     binding = DataBindingUtil.setContentView(this, R.layout.activity_main_updated) ;
       view = binding.getRoot();
       mainDashBoardViewModel = new ViewModelProvider(this).get(MainDashBoardViewModel.class);
       binding.setViewModel(mainDashBoardViewModel);
     }
+
+    private void setUpDashboardData(){
+
+        //for Date
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy, EEEE", Locale.ENGLISH);
+        String formattedDate = currentDate.format(formatter);
+        binding.txtDate.setText(formattedDate);
+    }
+
 
     private void onClickListener() {
         binding.labelMarketing.setOnClickListener(new View.OnClickListener() {
