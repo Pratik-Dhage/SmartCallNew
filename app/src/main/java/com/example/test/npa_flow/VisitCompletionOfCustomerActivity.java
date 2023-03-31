cc51b84533997905c816781e8acddb119b7fdfd9
Pratik Dhage <dhagepratik94@gmail.com>
2023-03-31 14:23:17 +0530

    Custom Dialog will appear on Clicking Notes and History icon

--- app/src/main/java/com/example/test/npa_flow/VisitCompletionOfCustomerActivity.java
+++ app/src/main/java/com/example/test/npa_flow/VisitCompletionOfCustomerActivity.java
@@ -1,11 +1,16 @@
 package com.example.test.npa_flow;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.test.R;
 import com.example.test.databinding.ActivityVisitCompletionOfCustomerBinding;
@@ -66,5 +71,62 @@ View view;
             Intent i = new Intent(this, BalanceInterestCalculationActivity.class);
             startActivity(i);
         });
+
+
+        //for Notes
+        binding.ivNotesIcon.setOnClickListener(v->{
+
+            View customDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog_box, null);
+
+            TextView customText =  customDialog.findViewById(R.id.txtCustomDialog);
+            Button customButton = customDialog.findViewById(R.id.btnCustomDialog);
+            EditText customEditBox = customDialog.findViewById(R.id.edtCustomDialog);
+            customEditBox.setVisibility(View.VISIBLE);
+
+            customText.setText(getResources().getString(R.string.lead_interaction));
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(customDialog);
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+
+            customButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+        });
+
+        //for History
+        binding.ivHistory.setOnClickListener(v->{
+
+            View customDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog_box, null);
+
+            TextView customText =  customDialog.findViewById(R.id.txtCustomDialog);
+            Button customButton = customDialog.findViewById(R.id.btnCustomDialog);
+            TextView txtCustom = customDialog.findViewById(R.id.txtCustom);
+            txtCustom.setVisibility(View.VISIBLE);
+
+            customText.setText(getResources().getString(R.string.lead_history));
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(customDialog);
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+
+            customButton.setText(R.string.close);
+            customButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+
+
+        });
+
     }
 }
