6f5d852bd4d078546509a62ff49856ea557aa581
Pratik Dhage <dhagepratik94@gmail.com>
2023-04-21 16:26:08 +0530

    Details of Customer will come in Recyclerview from Api in PaymentModeStatusActivity

--- app/src/main/java/com/example/test/npa_flow/PaymentModeStatusActivity.java
+++ app/src/main/java/com/example/test/npa_flow/PaymentModeStatusActivity.java
@@ -5,6 +5,8 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
@@ -26,6 +28,9 @@ import com.example.test.R;
 import com.example.test.databinding.ActivityPaymentModeStatusBinding;
 import com.example.test.fragments_activity.BalanceInterestCalculationActivity;
 import com.example.test.helper_classes.Global;
+import com.example.test.helper_classes.NetworkUtilities;
+import com.example.test.npa_flow.details_of_customer.DetailsOfCustomerViewModel;
+import com.example.test.npa_flow.details_of_customer.adapter.DetailsOfCustomerAdapter;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -38,41 +43,83 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
     View customDialogImagePicker;
     private ActivityResultLauncher<Intent> pickImageLauncher;
     private static final int PICK_IMAGE_REQUEST = 1;
+    DetailsOfCustomerViewModel detailsOfCustomerViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  setContentView(R.layout.activity_payment_mode_status);
+        //  setContentView(R.layout.activity_payment_mode_status);
 
         initializeFields();
-        getDetailsOfCustomerFromIntent();
+        initObserver();
+        if (NetworkUtilities.getConnectivityStatus(this)) {
+            callDetailsOfCustomerApi();
+        } else {
+            Global.showToast(this, getString(R.string.check_internet_connection));
+        }
         onClickListener();
         setUpImagePicker();
     }
 
     private void initializeFields() {
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_payment_mode_status);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_payment_mode_status);
         view = binding.getRoot();
+        detailsOfCustomerViewModel = new ViewModelProvider(this).get(DetailsOfCustomerViewModel.class);
+        binding.setViewModel(detailsOfCustomerViewModel);
 
     }
 
-    private void getDetailsOfCustomerFromIntent(){
+    private void callDetailsOfCustomerApi() {
 
-        binding.txtName.setText(getIntent().getStringExtra("name"));
-        binding.txtMobileNumber.setText(getIntent().getStringExtra("mobile_no"));
-        binding.txtAadharNumber.setText(getIntent().getStringExtra("aadhaar_no"));
-        binding.txtDOB.setText(getIntent().getStringExtra("dob"));
-        binding.txtFatherName.setText(getIntent().getStringExtra("father_name"));
-        binding.txtLoanAccountNumber.setText(getIntent().getStringExtra("loan_acc_no"));
-        binding.txtProduct.setText(getIntent().getStringExtra("product"));
-        binding.txtAmountDueAsOnAmount.setText(getIntent().getStringExtra("amt_due"));
-        binding.txtTotalAmountPaid.setText(getIntent().getStringExtra("total_amt_paid"));
-        binding.txtBalanceInterest.setText(getIntent().getStringExtra("balance_interest"));
-        binding.txtTotalPayableAmount.setText(getIntent().getStringExtra("total_payable_amt"));
+        String dataSetId = getIntent().getStringExtra("dataSetId");
+        detailsOfCustomerViewModel.getDetailsOfCustomer_Data(dataSetId); // call Details Of Customer API
+    }
+
+
+    private void setUpDetailsOfCustomerRecyclerView() {
+
+        detailsOfCustomerViewModel.updateDetailsOfCustomer_Data();
+        RecyclerView recyclerView = binding.rvDetailsOfCustomer;
+        recyclerView.setAdapter(new DetailsOfCustomerAdapter(detailsOfCustomerViewModel.arrList_DetailsOfCustomer_Data));
+    }
+
+    private void initObserver() {
+
+        if (NetworkUtilities.getConnectivityStatus(this)) {
+
+            binding.loadingProgressBar.setVisibility(View.VISIBLE);
+
+            detailsOfCustomerViewModel.getMutDetailsOfCustomer_ResponseApi().observe(this, result -> {
+
+                if (result != null) {
+
+                    detailsOfCustomerViewModel.arrList_DetailsOfCustomer_Data.clear();
+                    setUpDetailsOfCustomerRecyclerView();
+                    detailsOfCustomerViewModel.arrList_DetailsOfCustomer_Data.addAll(result);
+                    binding.loadingProgressBar.setVisibility(View.INVISIBLE);
+
+
+                }
+            });
+
+            //handle  error response
+            detailsOfCustomerViewModel.getMutErrorResponse().observe(this, error -> {
+
+                if (error != null && !error.isEmpty()) {
+                    Global.showSnackBar(view, error);
+                    System.out.println("Here: " + error);
+                } else {
+                    Global.showSnackBar(view, getResources().getString(R.string.check_internet_connection));
+                }
+            });
+        } else {
+            Global.showToast(this, getString(R.string.check_internet_connection));
+        }
 
     }
 
-    private void setUpImagePicker(){
+
+    private void setUpImagePicker() {
         // Initialize the ActivityResultLauncher
         pickImageLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
             if (result.getResultCode() == RESULT_OK && result.getData() != null) {
@@ -95,11 +142,13 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
                 ivFileUpload.setVisibility(View.GONE);
                 btnUploadReceipt.setVisibility(View.INVISIBLE);
 
-                txtProceed.setOnClickListener(v->{
-                    startActivity(new Intent(this,VisitCompletionOfCustomerActivity.class));
+                txtProceed.setOnClickListener(v -> {
+                    Intent i = new Intent(this, VisitCompletionOfCustomerActivity.class);
+                    i.putExtra("dataSetId", getIntent().getStringExtra("dataSetId"));
+                    startActivity(i);
                 });
 
-                ivRefreshCancel.setOnClickListener(v->{
+                ivRefreshCancel.setOnClickListener(v -> {
                     btnUploadReceipt.performClick();
                 });
             }
@@ -131,8 +180,6 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
     }
 
 
-
-
     private void onClickListener() {
 
         binding.ivBack.setOnClickListener(new View.OnClickListener() {
@@ -144,8 +191,8 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
 
         binding.btnWillPayLater.setOnClickListener(v -> {
 
-            Intent i = new Intent(this,ScheduleVisitForCollectionActivity.class);
-            i.putExtra("isFromPaymentModeStatusActivity","isFromPaymentModeStatusActivity");
+            Intent i = new Intent(this, ScheduleVisitForCollectionActivity.class);
+            i.putExtra("isFromPaymentModeStatusActivity", "isFromPaymentModeStatusActivity");
             startActivity(i);
 
         });
@@ -158,7 +205,6 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
             Button btnUploadReceipt = customDialogImagePicker.findViewById(R.id.btnUploadReceipt);
 
 
-
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setView(customDialogImagePicker);
             final AlertDialog dialog = builder.create();
@@ -168,7 +214,7 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
             dialog.show();
 
 
-            btnUploadReceipt.setOnClickListener(v2->{
+            btnUploadReceipt.setOnClickListener(v2 -> {
 
                 // Open gallery to pick an image
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -178,11 +224,13 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
 
             });
 
-            txtSkipAndProceed.setOnClickListener(v1->{
-                startActivity(new Intent(this,VisitCompletionOfCustomerActivity.class));
+            txtSkipAndProceed.setOnClickListener(v1 -> {
+                Intent i = new Intent(this, VisitCompletionOfCustomerActivity.class);
+                i.putExtra("dataSetId", getIntent().getStringExtra("dataSetId"));
+                startActivity(i);
             });
 
-            ivCancel.setOnClickListener(v1->{
+            ivCancel.setOnClickListener(v1 -> {
                 dialog.dismiss();
             });
 
@@ -198,7 +246,6 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
             Button btnUploadReceipt = customDialogImagePicker.findViewById(R.id.btnUploadReceipt);
 
 
-
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setView(customDialogImagePicker);
             final AlertDialog dialog = builder.create();
@@ -208,7 +255,7 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
             dialog.show();
 
 
-            btnUploadReceipt.setOnClickListener(v2->{
+            btnUploadReceipt.setOnClickListener(v2 -> {
 
                 // Open gallery to pick an image
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -218,11 +265,13 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
 
             });
 
-            txtSkipAndProceed.setOnClickListener(v1->{
-                startActivity(new Intent(this,VisitCompletionOfCustomerActivity.class));
+            txtSkipAndProceed.setOnClickListener(v1 -> {
+                Intent i = new Intent(this, VisitCompletionOfCustomerActivity.class);
+                i.putExtra("dataSetId", getIntent().getStringExtra("dataSetId"));
+                startActivity(i);
             });
 
-            ivCancel.setOnClickListener(v1->{
+            ivCancel.setOnClickListener(v1 -> {
                 dialog.dismiss();
             });
 
@@ -230,73 +279,12 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
         });
 
 
-
-
-
-        binding.btnNearBy.setOnClickListener(v->{
-            Intent i = new Intent(this, NearByCustomersActivity.class);
-            startActivity(i);
-        });
-
-        binding.btnCalculate.setOnClickListener(v->{
-            Intent i = new Intent(this, BalanceInterestCalculationActivity.class);
-            startActivity(i);
-        });
-
-
-        //for Editing Time
-        binding.ivEditTime.setOnClickListener(v->{
-
-            View customDialog2 = LayoutInflater.from(this).inflate(R.layout.custom_dialog_timepicker, null);
-            Button customButton = customDialog2.findViewById(R.id.btnOK);
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setView(customDialog2);
-            final AlertDialog dialog = builder.create();
-            dialog.setCancelable(false);
-            dialog.show();
-
-            //on Clicking OK button
-            customButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    // get a reference to the TimePicker
-                    TimePicker timePicker = customDialog2.findViewById(R.id.customDialogTimePicker);
-
-                    // get the selected hour and minute
-                    int hour = timePicker.getHour();
-                    int minute = timePicker.getMinute();
-
-                    // create a Date object with the selected time
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.set(Calendar.HOUR_OF_DAY, hour);
-                    calendar.set(Calendar.MINUTE, minute);
-                    Date selectedTime = calendar.getTime();
-
-                    // create a SimpleDateFormat object with the desired format string
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm a");
-
-                    // format the time and store it in a string
-                    String formattedTime = dateFormat.format(selectedTime);
-
-                    // display the selected time
-                    binding.txtScheduledTime.setText(":"+formattedTime);
-
-                    dialog.dismiss();
-                }
-            });
-
-        });
-
-
-
         //for Notes
-        binding.ivNotesIcon.setOnClickListener(v->{
+        binding.ivNotesIcon.setOnClickListener(v -> {
 
             View customDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog_box, null);
 
-            TextView customText =  customDialog.findViewById(R.id.txtCustomDialog);
+            TextView customText = customDialog.findViewById(R.id.txtCustomDialog);
             Button customButton = customDialog.findViewById(R.id.btnCustomDialog);
             EditText customEditBox = customDialog.findViewById(R.id.edtCustomDialog);
             customEditBox.setVisibility(View.VISIBLE);
@@ -318,11 +306,11 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
         });
 
         //for History
-        binding.ivHistory.setOnClickListener(v->{
+        binding.ivHistory.setOnClickListener(v -> {
 
             View customDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog_box, null);
 
-            TextView customText =  customDialog.findViewById(R.id.txtCustomDialog);
+            TextView customText = customDialog.findViewById(R.id.txtCustomDialog);
             Button customButton = customDialog.findViewById(R.id.btnCustomDialog);
             TextView txtCustom = customDialog.findViewById(R.id.txtCustom);
             txtCustom.setVisibility(View.VISIBLE);
@@ -345,8 +333,6 @@ public class PaymentModeStatusActivity extends AppCompatActivity {
         });
 
     }
-
-
 
 
 }
