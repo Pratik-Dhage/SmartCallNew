f3968435b9ebb394acb53f1e0c4bcf81e8dac296
Pratik Dhage <dhagepratik94@gmail.com>
2023-04-24 12:32:22 +0530

    CustomerDetailsActivity on ClickListener code added for history icon , notes icon and back arrow

--- app/src/main/java/com/example/test/fragments_activity/CustomerDetailsActivity.java
+++ app/src/main/java/com/example/test/fragments_activity/CustomerDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.test.fragments_activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
@@ -7,7 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.test.R;
 import com.example.test.databinding.ActivityCustomerDetailsBinding;
@@ -111,8 +116,70 @@ public class CustomerDetailsActivity extends AppCompatActivity {
             }
         });
 
+        binding.ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
 
+
+        //for Notes
+        binding.ivNotesIcon.setOnClickListener(v->{
+
+            View customDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog_box, null);
+
+            TextView customText =  customDialog.findViewById(R.id.txtCustomDialog);
+            Button customButton = customDialog.findViewById(R.id.btnCustomDialog);
+            EditText customEditBox = customDialog.findViewById(R.id.edtCustomDialog);
+            customEditBox.setVisibility(View.VISIBLE);
+
+            customText.setText(getResources().getString(R.string.lead_interaction));
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(customDialog);
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+
+            customButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+        });
+
+        //for History
+        binding.ivHistory.setOnClickListener(v->{
+
+            View customDialog = LayoutInflater.from(this).inflate(R.layout.custom_dialog_box, null);
+
+            TextView customText =  customDialog.findViewById(R.id.txtCustomDialog);
+            Button customButton = customDialog.findViewById(R.id.btnCustomDialog);
+            TextView txtCustom = customDialog.findViewById(R.id.txtCustom);
+            txtCustom.setVisibility(View.VISIBLE);
+
+
+            customText.setText(getResources().getString(R.string.lead_history));
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(customDialog);
+            final AlertDialog dialog = builder.create();
+            dialog.show();
+
+            customButton.setText(R.string.close);
+            customButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+
+
+        });
 
 
     }
