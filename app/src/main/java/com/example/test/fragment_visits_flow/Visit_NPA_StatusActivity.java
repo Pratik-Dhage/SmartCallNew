08f9dd2cf9fca2a272c7d3426071eb66d28b98bf
Pratik Dhage <dhagepratik94@gmail.com>
2023-04-25 15:12:54 +0530

    Visit_NPA_StatusActivity, activity_visit_npa_status added

--- app/src/main/java/com/example/test/fragment_visits_flow/Visit_NPA_StatusActivity.java
+++ app/src/main/java/com/example/test/fragment_visits_flow/Visit_NPA_StatusActivity.java
@@ -1,5 +1,7 @@
-package com.example.test.fragments_activity;
+package com.example.test.fragment_visits_flow;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -7,34 +9,43 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.test.R;
-import com.example.test.databinding.ActivityCustomerDetailsBinding;
-import com.example.test.fragment_visits_flow.Visit_NPA_StatusActivity;
+import com.example.test.databinding.ActivityVisitNpaStatusBinding;
 import com.example.test.helper_classes.Global;
 import com.example.test.helper_classes.NetworkUtilities;
-import com.example.test.npa_flow.NearByCustomersActivity;
+import com.example.test.npa_flow.ScheduleVisitForCollectionActivity;
+import com.example.test.npa_flow.VisitCompletionOfCustomerActivity;
 import com.example.test.npa_flow.details_of_customer.DetailsOfCustomerViewModel;
 import com.example.test.npa_flow.details_of_customer.adapter.DetailsOfCustomerAdapter;
 
-public class CustomerDetailsActivity extends AppCompatActivity {
+public class Visit_NPA_StatusActivity extends AppCompatActivity {
 
-    // This is from Visits Flow
 
-    ActivityCustomerDetailsBinding binding;
+    ActivityVisitNpaStatusBinding binding;
     View view;
+    View customDialogImagePicker;
+    private ActivityResultLauncher<Intent> pickImageLauncher;
+
     DetailsOfCustomerViewModel detailsOfCustomerViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_customer_details);
+        setContentView(R.layout.activity_visit_npa_status);
+
 
         initializeFields();
         initObserver();
@@ -45,11 +56,13 @@ public class CustomerDetailsActivity extends AppCompatActivity {
             Global.showToast(this,getString(R.string.check_internet_connection));
         }
         onClickListener();
+        setUpImagePicker();
+
     }
 
     private void initializeFields() {
 
-        binding= DataBindingUtil.setContentView(this,R.layout.activity_customer_details);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_visit_npa_status);
         view = binding.getRoot();
         detailsOfCustomerViewModel = new ViewModelProvider(this).get(DetailsOfCustomerViewModel.class);
         binding.setViewModel(detailsOfCustomerViewModel);
@@ -61,13 +74,13 @@ public class CustomerDetailsActivity extends AppCompatActivity {
         detailsOfCustomerViewModel.getDetailsOfCustomer_Data(dataSetId); // call Details Of Customer API
     }
 
-
     private void setUpDetailsOfCustomerRecyclerView(){
 
         detailsOfCustomerViewModel.updateDetailsOfCustomer_Data();
         RecyclerView recyclerView = binding.rvDetailsOfCustomer;
         recyclerView.setAdapter(new DetailsOfCustomerAdapter(detailsOfCustomerViewModel.arrList_DetailsOfCustomer_Data));
     }
+
 
     private void initObserver(){
 
@@ -78,14 +91,6 @@ public class CustomerDetailsActivity extends AppCompatActivity {
             detailsOfCustomerViewModel.getMutDetailsOfCustomer_ResponseApi().observe(this,result->{
 
                 if(result!=null) {
-
-                    //for Hiding Amount Paid ONLY in Details Of Customer Activity and Customer Details Activity
-                    result.iterator().forEachRemaining(it->{
-                        if(it.getSequence()==16 || it.getLable().contentEquals("Amount Paid")){
-                            it.setLable("");
-                            it.setEditable("");
-                        }
-                    });
 
                     detailsOfCustomerViewModel.arrList_DetailsOfCustomer_Data.clear();
                     setUpDetailsOfCustomerRecyclerView();
@@ -115,17 +120,69 @@ public class CustomerDetailsActivity extends AppCompatActivity {
     }
 
 
-    private void onClickListener() {
+    private void setUpImagePicker() {
+        // Initialize the ActivityResultLauncher
+        pickImageLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                // Get the file URI and file name
+                Uri uri = result.getData().getData();
+                String fileName = getFileNameFromUri(uri);
 
-        binding.btnVisitedTheCustomer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(CustomerDetailsActivity.this, Visit_NPA_StatusActivity.class);
-                String dataSetId = getIntent().getStringExtra("dataSetId");
-                i.putExtra("dataSetId",dataSetId);
-                startActivity(i);
+                // Set the file name on the TextView
+                TextView txtUploadReceipt = customDialogImagePicker.findViewById(R.id.txtUploadReceipt);
+                TextView txtProceed = customDialogImagePicker.findViewById(R.id.txtProceed);
+                TextView txtSkipAndProceed = customDialogImagePicker.findViewById(R.id.txtSkipAndProceed);
+                Button btnUploadReceipt = customDialogImagePicker.findViewById(R.id.btnUploadReceipt);
+                ImageView ivRefreshCancel = customDialogImagePicker.findViewById(R.id.ivRefreshCancel);
+                ImageView ivFileUpload = customDialogImagePicker.findViewById(R.id.ivFileUpload);
+
+                txtUploadReceipt.setText(fileName);
+                txtProceed.setVisibility(View.VISIBLE);
+                ivRefreshCancel.setVisibility(View.VISIBLE);
+                txtSkipAndProceed.setVisibility(View.GONE);
+                ivFileUpload.setVisibility(View.GONE);
+                btnUploadReceipt.setVisibility(View.INVISIBLE);
+
+                txtProceed.setOnClickListener(v -> {
+                    Intent i = new Intent(this, VisitCompletionOfCustomerActivity.class);
+                    i.putExtra("dataSetId", getIntent().getStringExtra("dataSetId"));
+                    startActivity(i);
+                });
+
+                ivRefreshCancel.setOnClickListener(v -> {
+                    btnUploadReceipt.performClick();
+                });
             }
         });
+    }
+
+    // Get the file name from the file URI
+    private String getFileNameFromUri(Uri uri) {
+        String fileName = null;
+        String scheme = uri.getScheme();
+        if (scheme != null && scheme.equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                if (columnIndex != -1) {
+                    fileName = cursor.getString(columnIndex);
+                }
+                cursor.close();
+            }
+        }
+        if (fileName == null) {
+            fileName = uri.getPath();
+            int cut = fileName.lastIndexOf('/');
+            if (cut != -1) {
+                fileName = fileName.substring(cut + 1);
+            }
+        }
+        return fileName;
+    }
+
+
+    private void onClickListener(){
+
 
         binding.ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,6 +192,51 @@ public class CustomerDetailsActivity extends AppCompatActivity {
         });
 
 
+        binding.btnAskedToVisitLater.setOnClickListener(v->{
+            Intent i = new Intent(this, ScheduleVisitForCollectionActivity.class);
+            startActivity(i);
+        });
+
+        binding.btnPaymentAlreadyMade.setOnClickListener(v->{
+
+            customDialogImagePicker = LayoutInflater.from(this).inflate(R.layout.custom_dialog_image_picker, null);
+            ImageView ivCancel = customDialogImagePicker.findViewById(R.id.ivCancel);
+            TextView txtSkipAndProceed = customDialogImagePicker.findViewById(R.id.txtSkipAndProceed);
+            Button btnUploadReceipt = customDialogImagePicker.findViewById(R.id.btnUploadReceipt);
+
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setView(customDialogImagePicker);
+            final AlertDialog dialog = builder.create();
+            dialog.setCancelable(true);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            dialog.show();
+
+
+            btnUploadReceipt.setOnClickListener(v2 -> {
+
+                // Open gallery to pick an image
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                pickImageLauncher.launch(Intent.createChooser(intent, "Select File"));
+
+
+            });
+
+            txtSkipAndProceed.setOnClickListener(v1 -> {
+                Intent i = new Intent(this, VisitCompletionOfCustomerActivity.class);
+                i.putExtra("dataSetId", getIntent().getStringExtra("dataSetId"));
+                startActivity(i);
+            });
+
+            ivCancel.setOnClickListener(v1 -> {
+                dialog.dismiss();
+            });
+
+
+
+        });
 
         //for Notes
         binding.ivNotesIcon.setOnClickListener(v->{
