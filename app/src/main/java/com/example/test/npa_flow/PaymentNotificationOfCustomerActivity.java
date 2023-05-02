e8378e98d42a6be68978888755c06a9bc56eaa99
Pratik Dhage <dhagepratik94@gmail.com>
2023-05-02 11:40:55 +0530

    PaymentNotificationOfCustomerActivity , navigate to DashBoard on Home Icon clicked

--- app/src/main/java/com/example/test/npa_flow/PaymentNotificationOfCustomerActivity.java
+++ app/src/main/java/com/example/test/npa_flow/PaymentNotificationOfCustomerActivity.java
@@ -23,6 +23,7 @@ import com.example.test.databinding.ActivityPaymentNotificationOfCustomerBinding
 import com.example.test.fragments_activity.BalanceInterestCalculationActivity;
 import com.example.test.helper_classes.Global;
 import com.example.test.helper_classes.NetworkUtilities;
+import com.example.test.main_dashboard.MainActivity3API;
 import com.example.test.npa_flow.details_of_customer.DetailsOfCustomerViewModel;
 import com.example.test.npa_flow.details_of_customer.adapter.DetailsOfCustomerAdapter;
 
@@ -116,6 +117,10 @@ public class PaymentNotificationOfCustomerActivity extends AppCompatActivity {
     private void onClickListener() {
 
         binding.ivBack.setOnClickListener(v -> onBackPressed());
+
+        binding.ivHome.setOnClickListener(v->{
+            startActivity(new Intent(this, MainActivity3API.class));
+        });
 
         binding.btnAskedToCallBackLater.setOnClickListener(v -> {
 
