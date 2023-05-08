d057a9bcfd6a0306a3f4615cceb4220e868a9407
Pratik Dhage <dhagepratik94@gmail.com>
2023-05-08 18:09:03 +0530

    EligibilityFragment , fragment_eligibility.xml changes

--- app/src/main/java/com/example/test/view_products/fragments/EligibilityFragment.java
+++ app/src/main/java/com/example/test/view_products/fragments/EligibilityFragment.java
@@ -2,6 +2,7 @@ package com.example.test.view_products.fragments;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,16 +10,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.test.R;
+import com.example.test.databinding.FragmentEligibilityBinding;
 
 public class EligibilityFragment extends Fragment {
 
-
+       FragmentEligibilityBinding binding;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eligibility, container, false);
+        
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_eligibility, container, false);
+        onClickListener();
+        
+        return binding.getRoot();
     }
+
+    private void onClickListener() {
+
+           binding.radioButtonSalaried.setOnClickListener(v->{
+               binding.radioButtonSalaried.setChecked(true);
+               binding.radioButtonSelfEmployed.setChecked(false);
+           });
+
+           binding.radioButtonSelfEmployed.setOnClickListener(v->{
+               binding.radioButtonSelfEmployed.setChecked(true);
+               binding.radioButtonSalaried.setChecked(false);
+           });
+    }
+
+
 }
