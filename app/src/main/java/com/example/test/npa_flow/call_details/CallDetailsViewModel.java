0c631186b6e2bb0fa2bc4b86ab51cba7e0e328f6
Pratik Dhage <dhagepratik94@gmail.com>
2023-05-22 18:09:56 +0530

    CallDetailsViewModel , made Two separate API methods for Full/Partial Amt, Paid flow and Will Pay Later flow for sendingCallLogList

--- app/src/main/java/com/example/test/npa_flow/call_details/CallDetailsViewModel.java
+++ app/src/main/java/com/example/test/npa_flow/call_details/CallDetailsViewModel.java
@@ -1,5 +1,7 @@
 package com.example.test.npa_flow.call_details;
 
+import android.content.Context;
+
 import androidx.core.app.ComponentActivity;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -7,6 +9,7 @@ import androidx.lifecycle.ViewModel;
 import com.example.test.api_manager.WebServices;
 import com.example.test.helper_classes.Global;
 import com.example.test.main_dashboard.model.DashBoardResponseModel;
+import com.example.test.npa_flow.ScheduleVisitForCollectionActivity;
 import com.example.test.npa_flow.details_of_customer.DetailsOfCustomerActivity;
 
 import java.util.List;
@@ -33,8 +36,10 @@ public class CallDetailsViewModel extends ViewModel {
 
     String userId = "CA_01_001";
 
-    List<CallDetails> callDetailsList = DetailsOfCustomerActivity.sendCallLogDetailsList();
-
+    DetailsOfCustomerActivity detailsOfCustomerActivity= new DetailsOfCustomerActivity();
+    List<CallDetails> callDetailsList = detailsOfCustomerActivity.sendCallLogDetailsList_WillPayLater(); //for Will Pay Later
+    List<CallDetails> callDetailsList_full_partial_amount = detailsOfCustomerActivity.sendCallLogDetailsList_FullPartialAmountPaid(); //for Full/Partial Amt. paid
+    String send_callScheduledTime = DetailsOfCustomerActivity.send_callScheduledTime;
     //WebServices.call_details_complete_no_change_partial_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId
 
     public void postCallDetails(String dataSetId , String payment_type, String complete_action){
@@ -48,7 +53,7 @@ public class CallDetailsViewModel extends ViewModel {
 
          if(complete_action.contentEquals("complete_no_change")){
 
-             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_no_change_partial_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId,callDetailsList)
+             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_no_change_partial_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId+"&scheduledDateTime="+"",callDetailsList_full_partial_amount)
                      .subscribeOn(Schedulers.io())
                      .observeOn(AndroidSchedulers.mainThread())
                      .unsubscribeOn(Schedulers.io())
@@ -59,7 +64,7 @@ public class CallDetailsViewModel extends ViewModel {
 
          if(complete_action.contentEquals("complete_need_to_update_details")){
 
-             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_need_to_update_details_partial_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId,callDetailsList)
+             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_need_to_update_details_partial_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId+"&scheduledDateTime="+"",callDetailsList_full_partial_amount)
                      .subscribeOn(Schedulers.io())
                      .observeOn(AndroidSchedulers.mainThread())
                      .unsubscribeOn(Schedulers.io())
@@ -69,7 +74,7 @@ public class CallDetailsViewModel extends ViewModel {
          }
 
          if(complete_action.contentEquals("complete_escalate_to_bm")) {
-             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_escalate_to_bm_partial_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId,callDetailsList)
+             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_escalate_to_bm_partial_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId+"&scheduledDateTime="+"",callDetailsList_full_partial_amount)
                      .subscribeOn(Schedulers.io())
                      .observeOn(AndroidSchedulers.mainThread())
                      .unsubscribeOn(Schedulers.io())
@@ -87,7 +92,7 @@ public class CallDetailsViewModel extends ViewModel {
 
          if(complete_action.contentEquals("complete_no_change")){
 
-             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_no_change_full_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId,callDetailsList)
+             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_no_change_full_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId+"&scheduledDateTime="+"",callDetailsList_full_partial_amount)
                      .subscribeOn(Schedulers.io())
                      .observeOn(AndroidSchedulers.mainThread())
                      .unsubscribeOn(Schedulers.io())
@@ -98,7 +103,7 @@ public class CallDetailsViewModel extends ViewModel {
 
          if(complete_action.contentEquals("complete_need_to_update_details")){
 
-             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_need_to_update_details_full_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId,callDetailsList)
+             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_need_to_update_details_full_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId+"&scheduledDateTime="+"",callDetailsList_full_partial_amount)
                      .subscribeOn(Schedulers.io())
                      .observeOn(AndroidSchedulers.mainThread())
                      .unsubscribeOn(Schedulers.io())
@@ -108,7 +113,7 @@ public class CallDetailsViewModel extends ViewModel {
          }
 
          if(complete_action.contentEquals("complete_escalate_to_bm")) {
-             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_escalate_to_bm_full_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId,callDetailsList)
+             subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_complete_escalate_to_bm_full_amt_paid+"&dataSetId="+dataSetId+"&callingAgent="+userId+"&scheduledDateTime="+"",callDetailsList_full_partial_amount)
                      .subscribeOn(Schedulers.io())
                      .observeOn(AndroidSchedulers.mainThread())
                      .unsubscribeOn(Schedulers.io())
@@ -121,6 +126,20 @@ public class CallDetailsViewModel extends ViewModel {
      }
 
 
+    }
+
+    //WILL PAY LATER
+    public void postScheduledDateTime(String dataSetId , String payment_type){
+        //WILL PAY LATER
+        if(payment_type.contentEquals("will pay later") ){
+            subscribtion = (Disposable) Global.apiService().post_call_details(WebServices.call_details_will_pay_later+"&dataSetId="+dataSetId+"&callingAgent="+userId+"&scheduledDateTime="+send_callScheduledTime,callDetailsList)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .unsubscribeOn(Schedulers.io())
+                    .subscribe(
+                            this::onHomeApiSuccess, this::onApiError
+                    );
+        }
     }
 
 
