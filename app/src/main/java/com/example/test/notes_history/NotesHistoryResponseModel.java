c0a70b737c5004277885405e2710a5258a4cf1a8
Pratik Dhage <dhagepratik94@gmail.com>
2023-06-17 12:30:07 +0530

    NotesHistoryResponseModel changes

--- app/src/main/java/com/example/test/notes_history/NotesHistoryResponseModel.java
+++ app/src/main/java/com/example/test/notes_history/NotesHistoryResponseModel.java
@@ -2,13 +2,49 @@ package com.example.test.notes_history;
 
 public class NotesHistoryResponseModel {
 
-    private String notesHistory;
+    private String userName;
+    private String date;
+    private String time;
+    private String activityType;
+    private String notes;
 
-    public String getNotesHistory() {
-        return notesHistory;
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getActivityType() {
+        return activityType;
+    }
+
+    public void setActivityType(String activityType) {
+        this.activityType = activityType;
     }
 
     public void setNotesHistory(String notesHistory) {
-        this.notesHistory = notesHistory;
+        this.notes = notes;
     }
 }
