ae82c9a22b5fabe6cf72c84c384900bc6978ed77
Pratik Dhage <dhagepratik94@gmail.com>
2023-07-06 16:46:49 +0530

    NotSpokeToCustomerActivity - method added storeCallDetailsInRoomDB() and used on buttons NoResponseBusy & NotReachableSwitchOff . After 3 call attempts to same customer User is navigated to SubmitCompletionActivityOfCustomer

--- app/src/main/java/com/example/test/npa_flow/NotSpokeToCustomerActivity.java
+++ app/src/main/java/com/example/test/npa_flow/NotSpokeToCustomerActivity.java
@@ -26,8 +26,10 @@ import com.example.test.npa_flow.details_of_customer.DetailsOfCustomerResponseMo
 import com.example.test.npa_flow.details_of_customer.DetailsOfCustomerViewModel;
 import com.example.test.npa_flow.details_of_customer.adapter.DetailsOfCustomerAdapter;
 import com.example.test.npa_flow.loan_collection.LoanCollectionActivity;
+import com.example.test.roomDB.dao.CallDetailsListDao;
 import com.example.test.roomDB.dao.LeadCallDao;
 import com.example.test.roomDB.database.LeadListDB;
+import com.example.test.roomDB.model.CallDetailsListRoomModel;
 import com.example.test.roomDB.model.LeadCallModelRoom;
 import com.example.test.schedule_flow.calls_for_the_day.CallsForTheDayActivity;
 
@@ -114,28 +116,51 @@ public class NotSpokeToCustomerActivity extends AppCompatActivity {
                 notSpokeToCustomer = true; // if Not Spoke To Customer is True Only then Show Call Attempts(Hands)
 
                 if(null!= DetailsOfCustomerActivity.FullName && null!= DetailsOfCustomerActivity.Mobile_Number){
-                    storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number);
+
+                    //if CallCount is becoming 4 then Navigate to SubmitCompletionActivityOfCustomer
+                    if(getCallCountFromRoomDB(DetailsOfCustomerActivity.Mobile_Number)==2){
+                        navigateToSubmitActivity("NoResponseBusy");
+                        storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number); //to remove hand gesture
+                        storeCallDetailsInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number,0); //to get 3 CallDateTime,CallDuration
+                    }
+
+                    else{
+                        storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number);
+                        storeCallDetailsInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number,0);
+                        Intent i = new Intent(this, CallsForTheDayActivity.class);
+                        startActivity(i);
+                    }
+
                 }
 
-                Intent i = new Intent(this, CallsForTheDayActivity.class);
-                startActivity(i);
+
             }
 
             //From NPA
             else{
-               System.out.println("Here isFromNPA_NotSpokeToCustomerActivity");
+                System.out.println("Here isFromNPA_NotSpokeToCustomerActivity");
                 notSpokeToCustomer = true; // if Not Spoke To Customer is True Only then Show Call Attempts(Hands)
 
                 if(null!= DetailsOfCustomerActivity.FullName && null!= DetailsOfCustomerActivity.Mobile_Number){
-                    storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number);
+
+                    //if CallCount is becoming 4 then Navigate to SubmitCompletionActivityOfCustomer
+                    if(getCallCountFromRoomDB(DetailsOfCustomerActivity.Mobile_Number)==2){
+                        navigateToSubmitActivity("NoResponseBusy");
+                        storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number); //to remove hand gesture
+                        storeCallDetailsInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number,0); //to get 3 CallDateTime,CallDuration
+                    }
+                    else{
+                        storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number);
+                        storeCallDetailsInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number,0);
+                        // Get DPD_row_position saved in SharedPreference in DPD_Adapter Class
+                        int DPD_row_position = Integer.parseInt(Global.getStringFromSharedPref(this, "DPD_row_position"));
+
+                        Intent i = new Intent(NotSpokeToCustomerActivity.this, LoanCollectionActivity.class);
+                        i.putExtra("DPD_row_position", DPD_row_position);
+                        startActivity(i);
+                    }
+
                 }
-
-                // Get DPD_row_position saved in SharedPreference in DPD_Adapter Class
-                int DPD_row_position = Integer.parseInt(Global.getStringFromSharedPref(this, "DPD_row_position"));
-
-                Intent i = new Intent(NotSpokeToCustomerActivity.this, LoanCollectionActivity.class);
-                i.putExtra("DPD_row_position", DPD_row_position);
-                startActivity(i);
 
             }
 
@@ -148,11 +173,24 @@ public class NotSpokeToCustomerActivity extends AppCompatActivity {
                 notSpokeToCustomer = true; // if Not Spoke To Customer is True Only then Show Call Attempts(Hands)
 
                 if(null!= DetailsOfCustomerActivity.FullName && null!= DetailsOfCustomerActivity.Mobile_Number){
-                    storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number);
+
+                    //if CallCount is becoming 4 then Navigate to SubmitCompletionActivityOfCustomer
+                    if(getCallCountFromRoomDB(DetailsOfCustomerActivity.Mobile_Number)==2){
+                        navigateToSubmitActivity("SwitchOff");
+                        storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number); //to remove hand gesture
+                        storeCallDetailsInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number,0); //to get 3 CallDateTime,CallDuration
+                    }
+                    else{
+
+                        storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number);
+                        storeCallDetailsInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number,0);
+                        Intent i = new Intent(this, CallsForTheDayActivity.class);
+                        startActivity(i);
+                    }
+
                 }
 
-                Intent i = new Intent(this, CallsForTheDayActivity.class);
-                startActivity(i);
+
             }
 
             //From NPA
@@ -161,18 +199,32 @@ public class NotSpokeToCustomerActivity extends AppCompatActivity {
                 notSpokeToCustomer = true; // if Not Spoke To Customer is True Only then Show Call Attempts(Hands)
 
                 if(null!= DetailsOfCustomerActivity.FullName && null!= DetailsOfCustomerActivity.Mobile_Number){
-                    storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number);
+
+                    //if CallCount is becoming 4 then Navigate to SubmitCompletionActivityOfCustomer
+                    if(getCallCountFromRoomDB(DetailsOfCustomerActivity.Mobile_Number)==2){
+                        navigateToSubmitActivity("SwitchOff");
+                        storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number); //to remove hand gesture
+                        storeCallDetailsInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number,0); //to get 3 CallDateTime,CallDuration
+                    }
+                    else{
+                        storeCallCountInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number);
+                        storeCallDetailsInRoomDB(DetailsOfCustomerActivity.FullName,DetailsOfCustomerActivity.Mobile_Number,0);
+
+                        // Get DPD_row_position saved in SharedPreference in DPD_Adapter Class
+                        int DPD_row_position = Integer.parseInt(Global.getStringFromSharedPref(this, "DPD_row_position"));
+
+                        Intent i = new Intent(NotSpokeToCustomerActivity.this, LoanCollectionActivity.class);
+                        i.putExtra("DPD_row_position", DPD_row_position);
+                        startActivity(i);
+                    }
+
                 }
 
-                // Get DPD_row_position saved in SharedPreference in DPD_Adapter Class
-                int DPD_row_position = Integer.parseInt(Global.getStringFromSharedPref(this, "DPD_row_position"));
-
-                Intent i = new Intent(NotSpokeToCustomerActivity.this, LoanCollectionActivity.class);
-                i.putExtra("DPD_row_position", DPD_row_position);
-                startActivity(i);
             }
 
         });
+
+
 
         binding.btnNumberIsInvalid.setOnClickListener(v -> {
             Intent i = new Intent(NotSpokeToCustomerActivity.this, SubmitCompletionActivityOfCustomer.class);
@@ -188,6 +240,52 @@ public class NotSpokeToCustomerActivity extends AppCompatActivity {
             i.putExtra("detailsList",detailsList);
             startActivity(i);
         });
+
+    }
+
+    public void navigateToSubmitActivity(String apiType){
+
+
+        if(apiType.contentEquals("NoResponseBusy")){
+            Intent i = new Intent(this, SubmitCompletionActivityOfCustomer.class);
+            i.putExtra("dataSetId", getIntent().getStringExtra("dataSetId"));
+            i.putExtra("detailsList",detailsList);
+            i.putExtra("isFromNotSpokeToCustomer_NoResponseBusy","isFromNotSpokeToCustomer_NoResponseBusy");
+            startActivity(i);
+
+        }
+        else if( apiType.contentEquals("SwitchOff")){
+            Intent i = new Intent(this, SubmitCompletionActivityOfCustomer.class);
+            i.putExtra("dataSetId", getIntent().getStringExtra("dataSetId"));
+            i.putExtra("detailsList",detailsList);
+            i.putExtra("isFromNotSpokeToCustomer_SwitchOff","isFromNotSpokeToCustomer_SwitchOff");
+            startActivity(i);
+        }
+
+    }
+
+    public int getCallCountFromRoomDB(String phoneNumber){
+        LeadCallDao leadCallDao = LeadListDB.getInstance(this).leadCallDao();
+        System.out.println("Here getCallCountFromRoomDB for :"+phoneNumber+" is "+leadCallDao.getCallCountUsingPhoneNumber(phoneNumber));
+        return leadCallDao.getCallCountUsingPhoneNumber(phoneNumber);
+    }
+
+    public void storeCallDetailsInRoomDB(String fullName ,String phoneNumber  ,int callDuration){
+        CallDetailsListDao callDetailsListDao = LeadListDB.getInstance(this).callDetailsListDao();
+
+        String pattern = "yyyy-MM-dd HH:mm:ss"; // Pattern to match the date format
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        try{
+            Date date = new Date();
+            String formattedCallDateTime = dateFormat.format(date);
+            CallDetailsListRoomModel callDetailsListRoomModel = new CallDetailsListRoomModel(fullName,phoneNumber,formattedCallDateTime,callDuration);
+            callDetailsListDao.insert(callDetailsListRoomModel);
+            // Will get 1st CallDateTime in Logcat for all 3 calls as it is fetching using same mobileNumber
+            System.out.println("Here CallDetailsDateTimeFromDB:"+callDetailsListDao.getCallDateTimeUsingMobileNumber(phoneNumber));
+        }
+        catch(Exception e ){
+            e.printStackTrace();
+        }
 
     }
 
@@ -208,7 +306,7 @@ public class NotSpokeToCustomerActivity extends AppCompatActivity {
 
         leadCallDao.UpdateLeadCalls(callCount, phoneNumber);
 
-        Global.showToast(this, "Call Count for " + phoneNumber + " is: " + leadCallDao.getCallCountUsingPhoneNumber(phoneNumber));
+       // Global.showToast(this, "Call Count for " + phoneNumber + " is: " + leadCallDao.getCallCountUsingPhoneNumber(phoneNumber));
         System.out.println("Here Call Count for " + phoneNumber + " is: " + leadCallDao.getCallCountUsingPhoneNumber(phoneNumber));
 
         DetailsOfCustomerActivity.send_callAttemptNo = callCount;
