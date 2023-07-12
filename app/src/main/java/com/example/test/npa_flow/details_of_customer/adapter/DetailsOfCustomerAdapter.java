80ce2b9e32506c4cf8a5744c5c1f7674e9cf2eea
Pratik Dhage <dhagepratik94@gmail.com>
2023-07-12 19:29:29 +0530

    DetailsOfCustomerAdapter - comment added

--- app/src/main/java/com/example/test/npa_flow/details_of_customer/adapter/DetailsOfCustomerAdapter.java
+++ app/src/main/java/com/example/test/npa_flow/details_of_customer/adapter/DetailsOfCustomerAdapter.java
@@ -460,7 +460,7 @@ public class DetailsOfCustomerAdapter extends RecyclerView.Adapter<DetailsOfCust
         }
 
         //for Button & Navigate Button(If Capture Button is Visible , then Navigate Button will also be Visible)
-        //Navigate & Capture Button Only visible for NPA & VisitsForTheDay
+        //Navigate & Capture Button Only visible for NPA & VisitsForTheDay using condition CallsForTheDayAdapter.isFromCallsForTheDayAdapter==null
         if (Objects.equals(a.getButton(), "Y") && CallsForTheDayAdapter.isFromCallsForTheDayAdapter==null) {
             holder.binding.btnDetail.setVisibility(View.VISIBLE);
             holder.binding.btnNavigate.setVisibility(View.VISIBLE);
