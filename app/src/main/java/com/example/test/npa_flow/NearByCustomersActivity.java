80bfac016a37dae4f3fe1263cd3fcb897d58c454
Pratik Dhage <dhagepratik94@gmail.com>
2023-07-27 13:42:21 +0530

    NearByCustomersActivity - on button click Visit NearBy Customer , navigated to NearByCustomerListActivity->CustomerDetailsActivity(Visits Flow)

--- app/src/main/java/com/example/test/npa_flow/NearByCustomersActivity.java
+++ app/src/main/java/com/example/test/npa_flow/NearByCustomersActivity.java
@@ -1,27 +1,37 @@
 package com.example.test.npa_flow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.test.R;
 import com.example.test.databinding.ActivityNearByCustomersBinding;
+import com.example.test.fragment_visits_flow.VisitsFlowViewModel;
 import com.example.test.helper_classes.Global;
 import com.example.test.main_dashboard.MainActivity3API;
 import com.example.test.npa_flow.details_of_customer.DetailsOfCustomerActivity;
 import com.example.test.npa_flow.loan_collection.LoanCollectionActivity;
+import com.example.test.npa_flow.nearby_customer.NearByCustomerListActivity;
+import com.example.test.npa_flow.nearby_customer.NearByCustomerViewModel;
 import com.example.test.schedule_flow.calls_for_the_day.CallsForTheDayActivity;
 import com.example.test.schedule_flow.calls_for_the_day.adapter.CallsForTheDayAdapter;
 import com.example.test.schedule_flow.visits_for_the_day.VisitsForTheDayActivity;
+import com.example.test.schedule_flow.visits_for_the_day.VisitsForTheDayViewModel;
 import com.example.test.schedule_flow.visits_for_the_day.adapter.VisitsForTheDayAdapter;
 
 public class NearByCustomersActivity extends AppCompatActivity {
 
     ActivityNearByCustomersBinding binding;
     View view;
+    NearByCustomerViewModel nearByCustomerViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,7 @@ public class NearByCustomersActivity extends AppCompatActivity {
     private void initializeFields() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_near_by_customers);
         view = binding.getRoot();
+        nearByCustomerViewModel = new ViewModelProvider(this).get(NearByCustomerViewModel.class);
 
         if(VisitsForTheDayAdapter.showNearByCustomerButton == true){
             binding.btnVisitNearbyCustomers.setVisibility(View.VISIBLE);
@@ -46,10 +57,6 @@ public class NearByCustomersActivity extends AppCompatActivity {
 
     private void onClickListener() {
 
-        binding.clLoanCollectionData.setOnClickListener(v->{
-            Intent i = new Intent(NearByCustomersActivity.this, DetailsOfCustomerActivity.class);
-            startActivity(i);
-        });
 
         binding.btnGotoDashBoard.setOnClickListener(v->{
             Intent i = new Intent(NearByCustomersActivity.this, MainActivity3API.class);
@@ -84,36 +91,33 @@ public class NearByCustomersActivity extends AppCompatActivity {
         });
 
         binding.btnVisitNearbyCustomers.setOnClickListener(v->{
-            Intent i = new Intent(this,LoanCollectionActivity.class);
-            i.putExtra("isFromNearByCustomerActivity","isFromNearByCustomerActivity");
-            startActivity(i);
-        });
 
 
-        //opens Google Maps
-        binding.ivMap1.setOnClickListener(v -> {
+            try {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+                } else if (!Global.isBackgroundLocationAccessEnabled(this)) {
+                    //request BackGroundLocation Access
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, Global.REQUEST_BACKGROUND_LOCATION);
+                    System.out.println("Here Requesting BackGroundLocation Permission");
+                }
 
-            //Below commented code is working in my device but not in Other devices
-           /* String location = "Navi Mumbai";
-            Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + location);  //geo:latitude,longitude?q=query  //geo:0,0?q=my+street+address
-          //  Uri gmmIntentUri = Uri.parse("geo:0,0?q=my+street+address"+ location);
-            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-            mapIntent.setPackage("com.google.android.apps.maps");
-            if (mapIntent.resolveActivity(getPackageManager()) != null) {
-                startActivity(mapIntent);
-            }*/
+                else if(Global.isLocationEnabled(this) && Global.isBackgroundLocationAccessEnabled(this)){
 
-            Intent googleMapsIntent = new Intent(NearByCustomersActivity.this,WebViewActivity.class);
-            startActivity(googleMapsIntent);
-        });
-
-        //opens Google Maps
-        binding.ivMap2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.ivMap1.performClick();
+                   Intent i = new Intent(this, NearByCustomerListActivity.class);
+                   startActivity(i);
+                }
             }
+            catch (Exception e){
+                System.out.println("NearByCustomer LocationException:"+e);
+            }
+
+
+
         });
+
+
+
 
 
     }
