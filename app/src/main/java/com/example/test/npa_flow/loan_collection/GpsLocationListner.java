9bd9b885a20c53b35947defea59205a3ddb78ee1
Pratik Dhage <dhagepratik94@gmail.com>
2023-08-21 13:32:17 +0530

    GpsLocationListner - in method onProviderEnabled() and onProviderDisabled() added System.out.println() that displays if GPS Provider/Network Provider is Enabled/Disabled to prevent app crash

--- app/src/main/java/com/example/test/npa_flow/loan_collection/GpsLocationListner.java
+++ app/src/main/java/com/example/test/npa_flow/loan_collection/GpsLocationListner.java
@@ -2,6 +2,7 @@ package com.example.test.npa_flow.loan_collection;
 
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -33,11 +34,30 @@ public class GpsLocationListner implements LocationListener {
 
     @Override
     public void onProviderEnabled(@NonNull String provider) {
-        LocationListener.super.onProviderEnabled(provider);
+       // LocationListener.super.onProviderEnabled(provider); //this line is causing crash on Turning Location ON
+
+        // When the location provider is enabled
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
+            // GPS provider is enabled
+            System.out.println("GPS Provider enabled");
+        } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+            // Network provider is enabled
+            System.out.println("Network Provider enabled");
+        }
     }
 
     @Override
     public void onProviderDisabled(@NonNull String provider) {
-        LocationListener.super.onProviderDisabled(provider);
+      //  LocationListener.super.onProviderDisabled(provider); //this line is causing crash on Turning Location OFF
+
+        //  When the location provider is disabled
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
+            // GPS provider is disabled
+            System.out.println("GPS Provider disabled");
+        } else if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+            // Network provider is disabled
+            System.out.println("Network Provider disabled");
+        }
+
     }
 }
